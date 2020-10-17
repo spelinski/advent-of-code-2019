196a0c70b78210c32d1b9e7f81560075b39cc1e5
spelinski <rohk88@gmail.com>
2020-10-17 15:53:53 -0500

    Edits after mypy and pylint runs

--- challenges/challenge1/challenge1.py
+++ challenges/challenge1/challenge1.py
@@ -1,26 +1,46 @@
+"""
+Solution to the first challenge of the 2019 Advent of Code
+"""
+
 from typing import List
 from math import floor
 
 def get_ints_from_file(input_filename: str) -> List[int]:
+    """
+    Read each line of the file as an integer into a list
+    """
     with open(input_filename) as input_file:
         return [int(line) for line in input_file]
 
-def get_fuel_needed_each_module(input_list: List[int]):
+def get_fuel_each_element(input_list: List[int]) -> List[int]:
+    """
+    List of fuel required to launch each element in the list
+    """
     return [floor(num/3)-2 for num in input_list if floor(num/3)-2 > 0]
 
+def get_fuel_ignore_fuel_weight(input_list: List[int]) -> int:
+    """
+    Total fuel needed to launch ignoring the fuel needed to launch the additional fuel
+    """
+    return sum(get_fuel_each_element(input_list))
+
+def get_fuel_include_fuel_weight(input_list: List[int]) -> int:
+    """
+    Total fuel needed to launch with additional fuel added to launch the fuel
+    """
+    fuel_needed = get_fuel_each_element(input_list)
+    fuel_needed_sum = 0
+    while fuel_needed:
+        fuel_needed_sum += sum(fuel_needed)
+        fuel_needed = get_fuel_each_element(fuel_needed)
+    return fuel_needed_sum
 
 if __name__ == "__main__":
     #Assume list gotten of every number
-    module_weights = get_ints_from_file("input.txt")
-    fuel_needed_each_module = get_fuel_needed_each_module(module_weights)
+    MODULE_WEIGHTS = get_ints_from_file("input.txt")
 
     print("Part 1 answer ignoring weight of fuel")
-    fuel_needed_total = sum(fuel_needed_each_module)
-    print(fuel_needed_total)
+    print(get_fuel_ignore_fuel_weight(MODULE_WEIGHTS))
 
-    fuel_needed_additional = get_fuel_needed_each_module(fuel_needed_each_module)
-    while(sum(fuel_needed_additional) > 0):
-        fuel_needed_total += sum(fuel_needed_additional)
-        fuel_needed_additional = get_fuel_needed_each_module(fuel_needed_additional)
     print("Part 2 answer with weight of fuel counted")
-    print(fuel_needed_total)            
+    print(get_fuel_include_fuel_weight(MODULE_WEIGHTS))
